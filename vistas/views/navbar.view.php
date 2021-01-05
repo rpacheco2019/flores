5b4a829d16e7e362bc1345df51a8409fb1f1e7ee
Roberto Pacheco <52426391+rpacheco2019@users.noreply.github.com>
2021-01-05 01:09:10 -0500

    Se agrega link a politica GP1 para compra de flores

--- vistas/views/navbar.view.php
+++ vistas/views/navbar.view.php
@@ -8,6 +8,9 @@
       <li class="nav-item d-none d-sm-inline-block">
         <a href="https://www.eodin.com/eventos/planner1/" class="nav-link">Easy Planner</a>
       </li>
+      <li class="nav-item d-none d-sm-inline-block">
+        <a href="https://www.notion.so/peeters/Pol-tica-de-Cotizaci-n-y-Compras-de-Art-culos-para-Producci-n-Floral-7a063077073a4a90b3e1225a8e16668e" class="nav-link" target="_blank" >Política</a>
+      </li>
       <!-- <li class="nav-item d-none d-sm-inline-block">
         <a href="#" class="nav-link">Contact</a>
       </li> -->
